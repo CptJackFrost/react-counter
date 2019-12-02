8c87767bd9c9acd39406cd5162741b0fd2faeb23
CptJackFrost <cptjackfrost74@gmail.com>
2019-12-02 22:42:14 +0500

    counter - number, 2 buttons (plus and minus)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './App.sass';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    }
+
+    this.values = [5, -6]
+
+    //this.onStatePlus = this.onStatePlus.bind(this) //common
+
+  }
+
+  onStateChange = (i) => {
+    this.setState(({count}) => {
+      return {
+        count: count + i
+      }
+    })
+  }
+
+  render() {    
+    const {count} = this.state;
+    return (      
+      <div className="App">
+        <div className="uk-card uk-card-default uk-card-body">
+          <h3 className="uk-card-title">Counter</h3>
+          <div>{count}</div>
+          <button 
+            onClick={()=> this.onStateChange(
+              +this.values.filter(item => item > 0)
+            )} 
+            className="uk-button uk-button-primary">
+              +
+          </button> 
+          <button 
+            onClick={()=> this.onStateChange(
+              +this.values.filter(item => item < 0)
+            )}
+            className="uk-button uk-button-secondary">
+              -
+            </button>
+        </div>
+      </div>
+    );
+
+  }
 }
 
 export default App;
